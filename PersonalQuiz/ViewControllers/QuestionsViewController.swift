d84d6dab49bcb250347c12b5955508a74d5c4424
amaya saudade <iwaizumi.yoshi@gmail.com>
2024-02-17 04:25:56 +0300

    build start screen

--- PersonalQuiz/ViewControllers/QuestionsViewController.swift
+++ PersonalQuiz/ViewControllers/QuestionsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  QuestionsViewController.swift
 //  PersonalQuiz
 //
 //  Created by Darina Kirilenko on 17.02.2024.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class QuestionsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
