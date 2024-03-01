d5cdd429211420354f1b511eca6e26b138f9a974
amaya saudade <iwaizumi.yoshi@gmail.com>
2024-03-02 02:37:56 +0300

    fix algorithm for getting result

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -12,45 +12,44 @@ final class ResultViewController: UIViewController {
     @IBOutlet var resultLabel: UILabel!
     @IBOutlet var descriptionLabel: UILabel!
     
-    var results: [Answer] = []
+    var results: [Answer]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let animalResult = getMostFrequentAnimal(in: results)
-        
-        resultLabel.text = "Вы - \(animalResult!.rawValue)"
-        descriptionLabel.text = animalResult!.definition
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
         navigationItem.hidesBackButton = true
     }
-
+    
     @IBAction func doneButtonAction(_ sender: UIBarButtonItem) {
         dismiss(animated: true)
     }
+}
     
-    deinit {
-        print("\(type(of: self)) has been deallocated")
-    }
-    
-    private func getMostFrequentAnimal(in results: [Answer]) -> Animal? {
+// MARK: - Private Methods
+extension ResultViewController {
+    private func getMostFrequentAnimal() {
         var frequencyDictionary: [Animal: Int] = [:]
+        let animals = results.map { $0.animal }
         
-        for answer in results {
-            frequencyDictionary[answer.animal, default: 0] += 1
-        }
-        
-        var mostFrequentAnimal: Animal?
-        var maxFrequency = 0
-        for (animal, frequency) in frequencyDictionary {
-            if frequency > maxFrequency {
-                mostFrequentAnimal = animal
-                maxFrequency = frequency
+        for animal in animals {
+            if let animalTypeCount = frequencyDictionary[animal] {
+                frequencyDictionary.updateValue(animalTypeCount + 1, forKey: animal)
+            } else {
+                frequencyDictionary[animal] = 1
             }
         }
-        return mostFrequentAnimal
+        
+        let sortedFrequentOfAnimals = frequencyDictionary.sorted { $0.value > $1.value }
+        guard let mostFrequentAnimal = sortedFrequentOfAnimals.first?.key else { return }
+        
+        updateUI(with: mostFrequentAnimal)
     }
+    
+    private func updateUI(with animal: Animal) {
+        resultLabel.text = "Вы - \(animal.rawValue)"
+        descriptionLabel.text = animal.definition
+    }
+       
 }
+    
+    
+
